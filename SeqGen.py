5357fe043848215b416e7174d927fe1ed91f07e1
Argenis Andrade <argenisandrade3b@gmail.com>
2023-12-31 00:54:14 -0500

    increased max lenght of sequences

--- SeqGen.py
+++ SeqGen.py
@@ -1,6 +1,6 @@
 import random
 
-def generate_random_amino_acid_sequence(min_length=10, max_length=50):
+def generate_random_amino_acid_sequence(min_length=10, max_length=80):
     amino_acids = 'ACDEFGHIKLMNPQRSTVWY'  # 20 standard amino acids
     length = random.randint(min_length, max_length)
     return ''.join(random.choice(amino_acids) for _ in range(length))
