876524365f686476feb1887e174e1a30dbb4052d
Argenis Andrade <argenisandrade3b@gmail.com>
2023-12-31 01:06:36 -0500

    decrease max lenght of sequences and rename first sequence variable

--- SeqGen.py
+++ SeqGen.py
@@ -1,13 +1,13 @@
 import random
 
-def generate_random_amino_acid_sequence(min_length=10, max_length=80):
+def generate_random_amino_acid_sequence(min_length=10, max_length=50):
     amino_acids = 'ACDEFGHIKLMNPQRSTVWY'  # 20 standard amino acids
     length = random.randint(min_length, max_length)
     return ''.join(random.choice(amino_acids) for _ in range(length))
 
 # Example usage
-random_sequence = generate_random_amino_acid_sequence()
-print(random_sequence)
+random_sequence1 = generate_random_amino_acid_sequence()
+print(random_sequence1)
 
 random_sequence2 = generate_random_amino_acid_sequence()
 print(random_sequence2)
