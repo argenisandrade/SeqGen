a6932baf74095ded1b872a638d878775f399b0d8
Argenis Andrade <argenisandrade3b@gmail.com>
2023-12-31 00:38:13 -0500

    generate 2 sequences

--- main.py
+++ main.py
@@ -8,3 +8,6 @@ def generate_random_amino_acid_sequence(min_length=10, max_length=50):
 # Example usage
 random_sequence = generate_random_amino_acid_sequence()
 print(random_sequence)
+
+random_sequence2 = generate_random_amino_acid_sequence()
+print(random_sequence2)
