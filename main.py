d580a60af400ea5779b007052e2bede617eeb14a
Argenis Andrade <argenisandrade3b@gmail.com>
2023-12-31 00:34:00 -0500

    first commit - commiting all files to the repository

---
+++ main.py
@@ -0,0 +1,10 @@
+import random
+
+def generate_random_amino_acid_sequence(min_length=10, max_length=50):
+    amino_acids = 'ACDEFGHIKLMNPQRSTVWY'  # 20 standard amino acids
+    length = random.randint(min_length, max_length)
+    return ''.join(random.choice(amino_acids) for _ in range(length))
+
+# Example usage
+random_sequence = generate_random_amino_acid_sequence()
+print(random_sequence)
